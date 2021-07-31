ba0ab515e584a53dd16100b2c4027588b1d4df19
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-07-31 17:19:01 +0530

    taliwind,redux,basicSetup-done

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store/store';
+
 
 export default function App() {
 	return (
-		<Provider>
+		<Provider  store={store} >
 			<View style={styles.container}>
-				<Text>Uber Clone Babe !!!</Text>
+				<HomeScreen />
 			</View>
 		</Provider>
 	);
