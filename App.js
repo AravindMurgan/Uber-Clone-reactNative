85ca37c756afaaf6e33860eaeae3e667d4ebcb05
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-07-31 16:26:59 +0530

    redux-setup

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Uber Clone Babe
-      </Text>
-  
-    </View>
-  );
+	return (
+		<Provider>
+			<View style={styles.container}>
+				<Text>Uber Clone Babe !!!</Text>
+			</View>
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
