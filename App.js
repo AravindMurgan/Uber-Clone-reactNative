e3f8b39f0e692af63ac3bffedd31a07bde70cd22
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-08-01 08:38:45 +0530

    lotsofdependency

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store/store';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
 	return (
 		<Provider store={store}>
+			<NavigationContainer>
 			<SafeAreaProvider>
 				<View style={styles.container}>
 					<HomeScreen />
 				</View>
 			</SafeAreaProvider>
+			</NavigationContainer>
 		</Provider>
 	);
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,41 +1,57 @@
 import React from 'react';
-import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+	FlatList,
+	Image,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from 'react-native';
+import { Icon } from 'react-native-elements';
 import tw from 'tailwind-react-native-classnames';
 
 
 const data = [
-    {
-        id:'123',
-        title:'Get a ride',
-        image:'https://links.papareact.com/3pn',
-        screen:'MapScreen',
-    },
-    {
-        id:'456',
-        title:'Order food',
-        image:'https://links.papareact.com/28w',
-        screen:'EatsScreen',
-    }
-]
+	{
+		id: '123',
+		title: 'Get a ride',
+		image: 'https://links.papareact.com/3pn',
+		screen: 'MapScreen',
+	},
+	{
+		id: '456',
+		title: 'Order food',
+		image: 'https://links.papareact.com/28w',
+		screen: 'EatsScreen',
+	},
+];
 
 const NavOptions = () => {
 	return (
 		<FlatList
-        data={data}
-        keyExtractor={(item)=> item.id}
-        renderItem={({item})=>(
-            <TouchableOpacity style={tw `p-2 pl-8 pb-8 bg-gray-200 m-2 w-40`} >
-                <View>
-                    <Image
-                    style={{width:120,height:120,resizeMode:'contain'}}
-                    source={{uri:item.image}}
-                    />
-                    <Text style={tw `mt-2 text-lg font-semibold text-black`} >{item.title}</Text>
-                </View>
-            </TouchableOpacity>
-        )}
-        horizontal
-        />
+			data={data}
+			keyExtractor={(item) => item.id}
+			renderItem={({ item }) => (
+				<TouchableOpacity style={tw `p-2 pl-8 pb-8 bg-gray-200 m-2 w-40`}>
+					<View>
+						<Image
+							style={{ width: 120, height: 120, resizeMode: 'contain' }}
+							source={{ uri: item.image }}
+						/>
+						<Text style={tw `mt-2 text-lg font-semibold text-black`}>
+							{item.title}
+						</Text>
+						<Icon
+							style={tw `rounded-full bg-black w-10 p-2 mt-4`}
+							name='arrowright'
+							color='white'
+							type='antdesign'
+						/>
+					</View>
+				</TouchableOpacity>
+			)}
+			horizontal
+		/>
 	);
 };
 
